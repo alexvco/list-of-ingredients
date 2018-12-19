962a11adeda516f499d5254e1b23765e0ba45b60
Alex Varjabedian <alex@trueandco.com>
2018-12-19 12:40:34 -0800

    add Drink model

---
+++ app/controllers/api/v1/drinks_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::DrinksController < ApiController
+  def index
+    @drinks = Drink.all
+    render json: @drinks.to_json
+  end
+
+  # GET /drinks/:id
+  def show
+    @drink = Drink.find(params[:id])
+    render json: @drink.to_json(:include => { :ingredients => { :only => [:id, :description] }})
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_19_193759) do
+ActiveRecord::Schema.define(version: 2018_12_19_203425) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2018_12_19_193759) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "drinks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "steps"
+    t.string "source"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
